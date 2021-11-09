Fixed cycle issues for Laplace + ideal vectors using local residual norm weighting of P LS fitting.

src/helmholtz/setup/interpolation.py
@@ -64,15 +64,26 @@ def create_interpolation_least_squares_domain(
 
     # Prepare fine and coarse test matrices.
     xc = r.dot(x)
+    residual = a.dot(x)
     if repetitive:
-        x_disjoint_aggregate_t, xc_disjoint_aggregate_t = \
-            hm.setup.sampling.get_disjoint_windows(x, xc, aggregate_size, nc, max_caliber)
+        x_disjoint_aggregate_t, xc_disjoint_aggregate_t, r_norm_disjoint_aggregate_t = \
+            hm.setup.sampling.get_disjoint_windows(x, xc, residual, aggregate_size, nc, max_caliber)
     else:
         x_disjoint_aggregate_t, xc_disjoint_aggregate_t = x.transpose(), xc.transpose()
+        # TODO(orenlivne): fix this to be all local residual norms in the non-repetitive case.
+        r_norm_disjoint_aggregate_t = None
+        # residual_window_size = 3 * aggregate_size  # Good for 1D.
+        # residual_window_offset = -(residual_window_size // 2)
+        # r_norm_disjoint_aggregate_t = np.concatenate(
+        # tuple(
+        #     np.linalg.norm(
+        #         hm.linalg.get_window(residual, offset + aggregate_size // 2 + residual_window_offset, residual_window_size),
+        #         axis=1) / residual_window_size ** 0.5
+        #     for offset in range(x.shape)), axis=1).transpose()
 
     if schema == "weighted":
         # Weighted LS: sum(w*(xc- x))^2 = sum(w^2*xc^2 - w*x^2).
-        weight = np.clip(hm.linalg.interval_norm(residual, residual_window_size), 1e-15, None) ** (-1)
+        weight = np.clip(r_norm_disjoint_aggregate_t, 1e-15, None) ** (-1)
     elif schema == "ridge":
         weight = None
     else:

src/helmholtz/setup/interpolation_ls_fit.py
@@ -293,7 +293,8 @@ def create_interpolation_least_squares_weighted(
     # Fit interpolation by unregularized weighted least-squares.
     p_coefficients = [np.linalg.lstsq(
         np.diag(weight[:, i]).dot(xc[:, nbhr_i]),
-        np.diag(weight[:, i]).dot(x[:, i])
+        np.diag(weight[:, i]).dot(x[:, i]),
+        rcond=None
     )[0] for i, nbhr_i in enumerate(nbhr)]
     return _create_csr_matrix(nbhr, p_coefficients, nc)
 

src/helmholtz/setup/sampling.py
@@ -3,27 +3,50 @@ import helmholtz as hm
 import numpy as np
 
 
-def get_disjoint_windows(x, xc, aggregate_size, nc, max_caliber):
+def get_disjoint_windows(x, xc, r, aggregate_size, nc, max_caliber):
     """
-     Creates windows of fine-level test functions on disjoint aggregates (e.g., points 0..3, 4..7, etc. for
-    aggregate_size = 4). No need to use all windows of the domain in the least-squares fit. It's sufficient to use
+    Samples windows of test functions.
+
+    No need to use all windows of the domain in the least-squares fit. It's sufficient to use
     than the interpolation caliber. Using 12 because we need (train, val, test) for LS fit, each of which is
     say 4 times the caliber to be safe.
+
+    :param x: fine-level test matrix.
+    :param xc: coarse-level test matrix.
+    :param r: fine-level residual matrix A*x.
+    :param aggregate_size: aggregate size.
+    :param nc: number of coarse variables per aggregate.
+    :param max_caliber: maximum caliber to consider. #windows is proportional to it.
+    :return:
+    - Windows of fine-level test functions 'x' on disjoint aggregates (e.g., points 0..3, 4..7, etc. for
+    aggregate_size = 4).
+    - Windows of coarse-level test functions on the same aggregates.
+    - Windows of local residual norms corresponding to the 'x' windows.
     """
     domain_size, num_test_functions = x.shape
     num_aggregates = int(np.ceil(domain_size / aggregate_size))
     num_windows = max(np.minimum(num_aggregates, (12 * max_caliber) // num_test_functions), 1)
-#    print("max_caliber", max_caliber, "num_test_functions", num_test_functions, "num_windows", num_windows, "num_aggregates", num_aggregates)
+    #    print("max_caliber", max_caliber, "num_test_functions", num_test_functions, "num_windows",
+    #    num_windows, "num_aggregates", num_aggregates)
+
+    # Create windows of 'x'.
     x_disjoint_aggregate_t = _get_disjoint_windows(x, aggregate_size, num_windows)
-    # Create corresponding windows of xc. Note: we are currently concatenating the entire coarse domain 'num_windows'
+
+    # Create corresponding windows of 'xc'. Note: we are currently concatenating the entire coarse domain 'num_windows'
     # times. This is not necessary if neighbor computation is done here and not inside create_interpolation(). For
     # now, we keep create_interpolation() general and do the nbhr computation there.
     # TODO(orenlivne): reduce storage here using smart periodic indexing or calculate the nbhr set here first
     # and only pass windows of nbhr values to create_interpolation.
-    num_coarse_vars = nc * num_aggregates
-    xc_disjoint_aggregate_t = np.concatenate(tuple(hm.linalg.get_window(xc, nc * offset, num_coarse_vars)
-                                                   for offset in range(num_windows)), axis=1).transpose()
-    return x_disjoint_aggregate_t, xc_disjoint_aggregate_t
+    xc_disjoint_aggregate_t = np.concatenate(
+        tuple(hm.linalg.get_window(xc, nc * offset, nc * num_aggregates)
+              for offset in range(num_windows)), axis=1).transpose()
+
+    # Create local residual norms corresponding to the 'x'-windows.
+    # TODO(orenlivne): in graph problems, replace residual_window_size by aggregate_size + sum of its residual_window_size
+    # aggregate sizes.
+    r_norm_disjoint_aggregate_t = _residual_norm_windows(r, aggregate_size, num_windows)
+
+    return x_disjoint_aggregate_t, xc_disjoint_aggregate_t, r_norm_disjoint_aggregate_t
 
 
 def _get_disjoint_windows(x, aggregate_size, num_windows):
@@ -31,3 +54,18 @@ def _get_disjoint_windows(x, aggregate_size, num_windows):
         tuple(hm.linalg.get_window(x, aggregate_size * offset, aggregate_size)
               for offset in range(num_windows)),
         axis=1).transpose()
+
+
+def _residual_norm_windows(r, aggregate_size, num_windows):
+    residual_window_size = 3 * aggregate_size  # Good for 1D.
+    residual_window_offset = -(residual_window_size // 2)
+    # Each residual window is centered at the center of the aggregate = offset + aggregate_size // 2, and
+    # extends ~0.5 * residual_window_size in each direction. Then the scaled L2 norm of window is calculated.
+    r_norm_disjoint_aggregate_t = np.concatenate(tuple(np.linalg.norm(
+        hm.linalg.get_window(r,
+                             (offset + aggregate_size // 2 + residual_window_offset) % r.shape[0],
+                             residual_window_size),
+        axis=0) for offset in range(num_windows))) / residual_window_size ** 0.5
+    # In principle, each point in the aggregate should have a slightly shifted residual window, but we just take the
+    # same residual norm for all points for simplicity. Should not matter much.
+    return np.tile(r_norm_disjoint_aggregate_t[:, None], (aggregate_size, ))

src/helmholtz/solve/solve_cycle.py
@@ -77,8 +77,8 @@ class SolutionCycleProcessor(hm.hierarchy.processor.Processor):
         self._print_state(l, "initial")
         level = self._multilevel[l]
         if self._nu_coarsest < 0:
-            #self._x[l] = spsolve(level.a, self._b[l])
-            self._x[l] = np.linalg.solve(level.a.todense(), self._b[l])
+            self._x[l] = spsolve(level.a, self._b[l])
+            #self._x[l] = np.linalg.solve(level.a.todense(), self._b[l])
             self._print_state(l, "exact")
         else:
             self._relax(l, self._nu_coarsest)

src/old_code/repetitive/bootstrap_repetitive.py
@@ -112,8 +112,9 @@ def bootstap(x, multilevel: Multilevel, max_levels: int,
     for l in range(1, max_levels):
         _LOGGER.info("Coarsening level {}->{}".format(l - 1, l))
         domain_size = level.a.shape[0]
-        r, p, _ = create_transfer_operators(x_level, domain_size, threshold=threshold, caliber=caliber,
-                                            interpolation_method=interpolation_method)
+        residual_level = level.operator(x_level)
+        r, p, _ = create_transfer_operators(x_level, residual_level, domain_size, threshold=threshold,
+                                            caliber=caliber, interpolation_method=interpolation_method)
         # 'level' now becomes the next coarser level and x_level the corresponding test matrix.
         level = hierarchy.create_tiled_coarse_level(level.a, level.b, r, p)
         new_multilevel.add(level)
@@ -170,13 +171,14 @@ def fmg(multilevel, nu_pre: int = 1, nu_post: int = 1, nu_coarsest: int = 10, nu
     return x
 
 
-def create_transfer_operators(x, domain_size: int, threshold: float = 0.1, caliber: int = 2,
+def create_transfer_operators(x, residual: np.ndarray, domain_size: int, threshold: float = 0.1, caliber: int = 2,
                               interpolation_method: str = "svd", max_coarsening_ratio: float = 0.5) -> \
         Tuple[Coarsener,Interpolator]:
     """
     Creates the next coarse level's R and P operators.
     Args:
         x: fine-level test matrix.
+        residual: fine-level residual matrix.
         domain_size: #gridpoints in fine level.
         threshold: relative reconstruction error threshold. Determines nc.
         caliber: interpolation caliber.
@@ -211,7 +213,7 @@ def create_transfer_operators(x, domain_size: int, threshold: float = 0.1, calib
     xc = r_csr.dot(x)
 
     x_disjoint_aggregate_t, xc_disjoint_aggregate_t = \
-        hm.setup.sampling.get_disjoint_windows(x, xc, aggregate_size, nc, caliber)
+        hm.setup.sampling.get_disjoint_windows(x, xc, residual, aggregate_size, nc, caliber)
 
     p = hm.setup.interpolation.create_interpolation_repetitive(
         interpolation_method, r.asarray(), x_disjoint_aggregate_t, xc_disjoint_aggregate_t, domain_size, nc)