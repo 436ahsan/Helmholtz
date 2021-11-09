WIP - fix weighting on P LS fitting. Need to match window of x to a neighborhood of A*x.

src/helmholtz/setup/interpolation.py
@@ -71,7 +71,8 @@ def create_interpolation_least_squares_domain(
         x_disjoint_aggregate_t, xc_disjoint_aggregate_t = x.transpose(), xc.transpose()
 
     if schema == "weighted":
-        weight = hm.linalg.interval_norm(x, residual_window_size)
+        # Weighted LS: sum(w*(xc- x))^2 = sum(w^2*xc^2 - w*x^2).
+        weight = np.clip(hm.linalg.interval_norm(residual, residual_window_size), 1e-15, None) ** (-1)
     elif schema == "ridge":
         weight = None
     else:

src/helmholtz/setup/interpolation_ls_fit.py
@@ -291,7 +291,10 @@ def create_interpolation_least_squares_weighted(
     assert len(nbhr) == n
 
     # Fit interpolation by unregularized weighted least-squares.
-    p_coefficients = [np.linalg.lstsq(xc[:, nbhr_i], x[:, i])[0] for i, nbhr_i in enumerate(nbhr)]
+    p_coefficients = [np.linalg.lstsq(
+        np.diag(weight[:, i]).dot(xc[:, nbhr_i]),
+        np.diag(weight[:, i]).dot(x[:, i])
+    )[0] for i, nbhr_i in enumerate(nbhr)]
     return _create_csr_matrix(nbhr, p_coefficients, nc)
 
 

src/helmholtz/setup/sampling.py
@@ -14,10 +14,7 @@ def get_disjoint_windows(x, xc, aggregate_size, nc, max_caliber):
     num_aggregates = int(np.ceil(domain_size / aggregate_size))
     num_windows = max(np.minimum(num_aggregates, (12 * max_caliber) // num_test_functions), 1)
 #    print("max_caliber", max_caliber, "num_test_functions", num_test_functions, "num_windows", num_windows, "num_aggregates", num_aggregates)
-    x_disjoint_aggregate_t = np.concatenate(
-        tuple(hm.linalg.get_window(x, aggregate_size * offset, aggregate_size)
-              for offset in range(num_windows)),
-        axis=1).transpose()
+    x_disjoint_aggregate_t = _get_disjoint_windows(x, aggregate_size, num_windows)
     # Create corresponding windows of xc. Note: we are currently concatenating the entire coarse domain 'num_windows'
     # times. This is not necessary if neighbor computation is done here and not inside create_interpolation(). For
     # now, we keep create_interpolation() general and do the nbhr computation there.
@@ -27,3 +24,10 @@ def get_disjoint_windows(x, xc, aggregate_size, nc, max_caliber):
     xc_disjoint_aggregate_t = np.concatenate(tuple(hm.linalg.get_window(xc, nc * offset, num_coarse_vars)
                                                    for offset in range(num_windows)), axis=1).transpose()
     return x_disjoint_aggregate_t, xc_disjoint_aggregate_t
+
+
+def _get_disjoint_windows(x, aggregate_size, num_windows):
+    return np.concatenate(
+        tuple(hm.linalg.get_window(x, aggregate_size * offset, aggregate_size)
+              for offset in range(num_windows)),
+        axis=1).transpose()

src/test/setup/test_interpolation.py
@@ -289,7 +289,7 @@ class TestInterpolation:
 
         error_a = np.mean(norm(a.dot(x - p.dot(r.dot(x))), axis=0) / norm(x, axis=0))
         assert p[0].nnz == 4
-        assert error_a == pytest.approx(0.129, 1e-2)
+        assert error_a == pytest.approx(0.132, 1e-2)
         assert p.shape == (32, 16)
 
 