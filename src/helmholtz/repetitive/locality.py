Fixed bug in residual norm subdomain.  Shows that  ideal TV P is sensitive to round-off, depends on exact weighting and luck.

src/helmholtz/repetitive/locality.py
@@ -103,7 +103,8 @@ def create_two_level_hierarchy_from_matrix(a, location, r, p, aggregate_size, nc
     return multilevel
 
 
-def two_level_conv_factor(multilevel, nu_pre, nu_post: int = 0, print_frequency: int = None, debug: bool = False):
+def two_level_conv_factor(multilevel, nu_pre, nu_post: int = 0, print_frequency: int = None, debug: bool = False,
+                          residual_stop_value: float = 1e-10):
     level = multilevel.finest_level
     n = level.size
     # Test two-level cycle convergence for A*x=b with b=A*x0, x0=random[-1, 1].
@@ -119,7 +120,8 @@ def two_level_conv_factor(multilevel, nu_pre, nu_post: int = 0, print_frequency:
         return b - multilevel[0].operator(x)
 
     return hm.solve.run.run_iterative_method(
-        residual, two_level_cycle, np.random.random((n, )), 20, print_frequency=print_frequency)
+        residual, two_level_cycle, np.random.random((n, )), 20, print_frequency=print_frequency,
+        residual_stop_value=residual_stop_value)
 
 
 def two_level_conv_data_frame(kh, discretization, r, p, aggregate_size, m_values, nu_values):

src/helmholtz/setup/sampling.py
@@ -39,7 +39,8 @@ def get_disjoint_windows(x, xc, r, aggregate_size: int, num_components: int, num
     # Create local residual norms corresponding to the 'x'-windows.
     # TODO(orenlivne): in graph problems, replace residual_window_size by aggregate_size + sum of its
     #  residual_window_size aggregate sizes.
-    r_norm_disjoint_aggregate_t = residual_norm_windows(r, aggregate_size, num_windows)
+    residual_window_size = 3 * aggregate_size  # Good for 1D.
+    r_norm_disjoint_aggregate_t = residual_norm_windows(r, aggregate_size, num_windows, residual_window_size)
 
     return x_disjoint_aggregate_t, xc_disjoint_aggregate_t, r_norm_disjoint_aggregate_t
 
@@ -59,16 +60,15 @@ def get_windows_by_index(x, index, stride, num_windows):
         axis=1).transpose()[:num_windows]
 
 
-def residual_norm_windows(r, aggregate_size, num_windows):
-    residual_window_size = 3 * aggregate_size  # Good for 1D.
-    residual_window_offset = -(residual_window_size // 2)
+def residual_norm_windows(r, aggregate_size, num_windows, residual_window_size):
     # Each residual window is centered at the center of the aggregate = offset + aggregate_size // 2, and
     # extends ~ 0.5 * residual_window_size in each direction. Then the scaled L2 norm of window is calculated.
-    r_norm_disjoint_aggregate_t = np.concatenate(tuple(np.linalg.norm(
-        hm.linalg.get_window(r,
-                             (offset + aggregate_size // 2 + residual_window_offset) % r.shape[0],
-                             residual_window_size),
-        axis=0) for offset in range(num_windows))) / residual_window_size ** 0.5
+    window_start = aggregate_size // 2 - (residual_window_size // 2)
+
+    r_windows = get_windows_by_index(
+        r, np.arange(window_start, window_start + residual_window_size), aggregate_size, num_windows * r.shape[1])
+    r_norm_disjoint_aggregate_t = np.linalg.norm(r_windows, axis=1) / residual_window_size ** 0.5
+
     # In principle, each point in the aggregate should have a slightly shifted residual window, but we just take the
     # same residual norm for all points for simplicity. Should not matter much.
     return np.tile(r_norm_disjoint_aggregate_t[:, None], (aggregate_size, ))

src/test/setup/test_interpolation.py
@@ -16,7 +16,7 @@ class TestInterpolation:
     def setup_method(self):
         """Fixed random seed for deterministic results."""
         np.random.seed(0)
-        hm.logging.set_simple_logging(logging.DEBUG)
+        hm.logging.set_simple_logging(logging.WARN)
 
     def test_create_coarsening_domain_non_repetitive(self):
         n = 32
@@ -322,7 +322,7 @@ class TestInterpolation:
 
         error_a = np.mean(norm(a.dot(x - p.dot(r.dot(x))), axis=0) / norm(x, axis=0))
         assert p[0].nnz == 4
-        assert error_a == pytest.approx(0.125, 1e-2)
+        assert error_a == pytest.approx(0.128, 1e-2)
         assert p.shape == (32, 16)
 
 